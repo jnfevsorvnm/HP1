5d41e7cd7e243cf1552f3d5384a7feb66c3e0590
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-04-11 20:34:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -450,6 +450,180 @@ message.delete();
 };     
 });
 
+var viper = ["https://f.top4top.net/p_682it2tg6.png%22","https://e.top4top.net/p_682a1cus5.png%22","https://d.top4top.net/p_682pycol4.png%22","https://c.top4top.net/p_682vqehy3.png%22","https://b.top4top.net/p_682mlf9d2.png%22","https://a.top4top.net/p_6827dule1.png%22","https://b.top4top.net/p_682g1meb10.png%22","https://a.top4top.net/p_682jgp4v9.png%22","https://f.top4top.net/p_682d4joq8.png%22","https://e.top4top.net/p_6828o0e47.png%22","https://d.top4top.net/p_6824x7sy6.png%22","https://c.top4top.net/p_682gzo2l5.png%22","https://b.top4top.net/p_68295qg04.png%22","https://a.top4top.net/p_682zrz6h3.png%22","https://f.top4top.net/p_6828vkzc2.png%22","https://e.top4top.net/p_682i8tb11.png",]
+    client.on('message', message => {
+        var args = message.content.split(" ").slice(1);
+if (message.content.startsWith('لو خيروك')) {
+         var lo = new Discord.RichEmbed()
+.setImage(viper[Math.floor(Math.random() * viper.length)])
+message.channel.sendEmbed(lo);
+    }
+});
+
+
+
+const secreT = [
+    "**الحياة بكل ما فيها تقف دائمًا على حد الوسطية بين اتزان المعنى وضده من حب وكره وحق وباطل وعدل وظلم**.",
+    "**كى تعيش عليك ان تتقن فن التجاهل باحتراف**.",
+    "**لا تحزن على من اشعرك بان طيبتك غباء امام وقاحته**.",
+    "**هناك من يحلم بالنجاح وهناك من يستيقظ باكرا لتحقيقه**.",
+    "**ان تعالج ألمك بنفسك تلك هى القوة**.", 
+    "**الجميع يسمع ما تقول والاصدقاء ينصتون لما تقول وافضل الاصدقاء ينصتون لما اخفاه سكوتك**.", 
+    "**انتهى زمن الفروسية ، لم تنقرض الخيول بل انقرض الفرسان**.", 
+    "**ان تكون اخرسا عاقلا خير من ان تكون نطوقا جهولا**.", 
+    "**المناقشات العقيمة لا تنجب افكارا**.", 
+    "**نحن نكتب ما لا نستطيع ان نقول وما نريد ان يكون**.", 
+    "**نحن نكتب ما لا نستطيع ان نقول وما نريد ان يكون**.", 
+  ]
+  
+  
+   client.on('message', message => {
+     if (message.content.startsWith("خواطر")) {
+                  if(!message.channel.guild) return message.reply('** This command only for servers**');
+    var embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+  
+     .setThumbnail(message.author.avatarURL) 
+   .addField('لعبه خواطر' ,
+    `${secreT[Math.floor(Math.random() * secreT.length)]}`)
+    message.channel.sendEmbed(embed);
+    console.log('[id] Send By: ' + message.author.username)
+      }
+  });
+
+
+
+const cuttweet = [
+    'كت تويت ‏| تخيّل لو أنك سترسم شيء وحيد فيصبح حقيقة، ماذا سترسم؟',
+    'كت تويت | أكثر شيء يُسكِت الطفل برأيك؟',
+    'كت تويت | الحرية لـ ... ؟',
+    'كت تويت | قناة الكرتون المفضلة في طفولتك؟',
+    'كت تويت ‏| كلمة للصُداع؟',
+    'كت تويت ‏| ما الشيء الذي يُفارقك؟',
+    'كت تويت | موقف مميز فعلته مع شخص ولا يزال يذكره لك؟',
+    'كت تويت ‏| أيهما ينتصر، الكبرياء أم الحب؟',
+    'كت تويت | بعد ١٠ سنين ايش بتكون ؟',
+    'كت تويت ‏| مِن أغرب وأجمل الأسماء التي مرت عليك؟',
+    '‏كت تويت | عمرك شلت مصيبة عن شخص برغبتك ؟',
+    'كت تويت | أكثر سؤال وجِّه إليك مؤخرًا؟',
+    '‏كت تويت | ما هو الشيء الذي يجعلك تشعر بالخوف؟',
+    '‏كت تويت | وش يفسد الصداقة؟',
+    '‏كت تويت | شخص لاترفض له طلبا ؟',
+    '‏كت تويت | كم مره خسرت شخص تحبه؟.',
+    '‏كت تويت | كيف تتعامل مع الاشخاص السلبيين ؟',
+    '‏كت تويت | كلمة تشعر بالخجل اذا قيلت لك؟',
+    '‏كت تويت | جسمك اكبر من عٌمرك او العكسّ ؟!',
+    '‏كت تويت |أقوى كذبة مشت عليك ؟',
+    '‏كت تويت | تتأثر بدموع شخص يبكي قدامك قبل تعرف السبب ؟',
+    'كت تويت | هل حدث وضحيت من أجل شخصٍ أحببت؟',
+    '‏كت تويت | أكثر تطبيق تستخدمه مؤخرًا؟',
+    '‏كت تويت | ‏اكثر شي يرضيك اذا زعلت بدون تفكير ؟',
+    '‏كت تويت | وش محتاج عشان تكون مبسوط ؟',
+    '‏كت تويت | مطلبك الوحيد الحين ؟',
+    '‏كت تويت | هل حدث وشعرت بأنك ارتكبت أحد الذنوب أثناء الصيام؟',
+]
+
+client.on('message', message => {
+  if (message.content.startsWith("كت تويت")) {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+ var embed = new Discord.RichEmbed()
+ .setColor('RANDOM')
+  .setThumbnail(message.author.avatarURL) 
+.addField('لعبه كت تويت' ,
+ `${cuttweet[Math.floor(Math.random() * cuttweet.length)]}`)
+ message.channel.sendEmbed(embed);
+ console.log('[id] Send By: ' + message.author.username)
+   }
+});
+
+
+   
+var cats = ["https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg","http://www.dogbazar.org/wp-content/uploads/2014/09/british-bull-dog-puppies.jpg","http://cdn2-www.dogtime.com/assets/uploads/gallery/german-shepherd-dog-breed-pictures/standing-7.jpg","http://cdn.akc.org/Marketplace/Breeds/German_Shepherd_Dog_SERP.jpg","https://animalso.com/wp-content/uploads/2016/12/black-german-shepherd_2.jpg","https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpg","https://www.petfinder.com/wp-content/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg","http://www.i-love-cats.com/images/2015/04/12/cat-wallpaper-38.jpg","https://www.aspca.org/sites/default/files/cat-care_urine-marking_main-image.jpg","https://s-media-cache-ak0.pinimg.com/originals/f0/3b/76/f03b7614dfadbbe4c2e8f88b69d12e04.jpg","http://www.rd.com/wp-content/uploads/sites/2/2016/04/15-cat-wants-to-tell-you-attention.jpg","https://www.thelocal.de/userdata/images/article/fa6fd5014ccbd8f4392f716473ab6ff354f871505d9128820bbb0461cce1d645.jpg","https://www.adelaidezoo.com.au/wp-content/uploads/sites/2/animals/GiantPanda3Slider.jpg","http://imagem.band.com.br/f_230168.jpg"]
+    client.on('message', message => {
+        var args = message.content.split(" ").slice(1);
+if (message.content.startsWith('حيوانات')) {
+         var cat = new Discord.RichEmbed()
+.setImage(cats[Math.floor(Math.random() * cats.length)])
+message.channel.sendEmbed(cat);
+    }
+});
+
+const Sra7a = [
+    'صراحه  |  صوتك حلوة؟',
+    'صراحه  |  التقيت الناس مع وجوهين؟',
+    'صراحه  |  شيء وكنت تحقق اللسان؟',
+    'صراحه  |  أنا شخص ضعيف عندما؟',
+    'صراحه  |  هل ترغب في إظهار حبك ومرفق لشخص أو رؤية هذا الضعف؟',
+    'صراحه  |  يدل على أن الكذب مرات تكون ضرورية شي؟',
+    'صراحه  |  أشعر بالوحدة على الرغم من أنني تحيط بك كثيرا؟',
+    'صراحه  |  كيفية الكشف عن من يكمن عليك؟',
+    'صراحه  |  إذا حاول شخص ما أن يكرهه أن يقترب منك ويهتم بك تعطيه فرصة؟',
+    'صراحه  |  أشجع شيء حلو في حياتك؟',
+    'صراحه  |  طريقة جيدة يقنع حتى لو كانت الفكرة خاطئة" توافق؟',
+    'صراحه  |  كيف تتصرف مع من يسيئون فهمك ويأخذ على ذهنه ثم ينتظر أن يرفض؟',
+    'صراحه  |  التغيير العادي عندما يكون الشخص الذي يحبه؟',
+    'صراحه  |  المواقف الصعبة تضعف لك ولا ترفع؟',
+    'صراحه  |  نظرة و يفسد الصداقة؟',
+    'صراحه  |  ‏‏إذا أحد قالك كلام سيء بالغالب وش تكون ردة فعلك؟',
+    'صراحه  |  شخص معك بالحلوه والمُره؟',
+    'صراحه  |  ‏هل تحب إظهار حبك وتعلقك بالشخص أم ترى ذلك ضعف؟',
+    'صراحه  |  تأخذ بكلام اللي ينصحك ولا تسوي اللي تبي؟',
+    'صراحه  |  وش تتمنى الناس تعرف عليك؟',
+    'صراحه  |  ابيع المجرة عشان؟',
+    'صراحه  |  أحيانا احس ان الناس ، كمل؟',
+    'صراحه  |  مع مين ودك تنام اليوم؟',
+    'صراحه  |  صدفة العمر الحلوة هي اني؟',
+    'صراحه  |  الكُره العظيم دايم يجي بعد حُب قوي " تتفق؟',
+    'صراحه  |  صفة تحبها في نفسك؟',
+    'صراحه  |  ‏الفقر فقر العقول ليس الجيوب " ، تتفق؟',
+    'صراحه  |  تصلي صلواتك الخمس كلها؟',
+    'صراحه  |  ‏تجامل أحد على راحتك؟',
+    'صراحه  |  اشجع شيء سويتة بحياتك؟',
+    'صراحه  |  وش ناوي تسوي اليوم؟',
+    'صراحه  |  وش شعورك لما تشوف المطر؟',
+    'صراحه  |  غيرتك هاديه ولا تسوي مشاكل؟',
+    'صراحه  |  ما اكثر شي ندمن عليه؟',
+    'صراحه  |  اي الدول تتمنى ان تزورها؟',
+    'صراحه  |  متى اخر مره بكيت؟',
+    'صراحه  |  تقيم حظك ؟ من عشره؟',                                                                                                                                            
+  'صراحه  |  هل تعتقد ان حظك سيئ؟',
+    'صراحه  |  شـخــص تتمنــي الإنتقــام منـــه؟',
+    'صراحه  |  كلمة تود سماعها كل يوم؟',
+    'صراحه  |  **هل تُتقن عملك أم تشعر بالممل؟',
+    'صراحه  |  هل قمت بانتحال أحد الشخصيات لتكذب على من حولك؟',
+    'صراحه  |  متى آخر مرة قمت بعمل مُشكلة كبيرة وتسببت في خسائر؟',
+    'صراحه  |  ما هو اسوأ خبر سمعته بحياتك؟',
+    '‏صراحه | هل جرحت شخص تحبه من قبل ؟',
+    'صراحه  |  ما هي العادة التي تُحب أن تبتعد عنها؟',
+    '‏صراحه | هل تحب عائلتك ام تكرههم؟',
+    '‏صراحه  |  من هو الشخص الذي يأتي في قلبك بعد الله – سبحانه وتعالى- ورسوله الكريم – صلى الله عليه وسلم؟',
+    '‏صراحه  |  هل خجلت من نفسك من قبل؟',
+    '‏صراحه  |  ما هو ا الحلم  الذي لم تستطيع ان تحققه؟',
+    '‏صراحه  |  ما هو الشخص الذي تحلم به كل ليلة؟',
+    '‏صراحه  |  هل تعرضت إلى موقف مُحرج جعلك تكره صاحبهُ؟',
+     '‏صراحه  |  هل قمت بالبكاء أمام من تُحب؟',
+    '‏صراحه  |  ماذا تختار حبيبك أم صديقك؟',
+    '‏صراحه  | هل حياتك سعيدة أم حزينة؟',
+    'صراحه  |  ما هي أجمل سنة عشتها بحياتك؟',
+    '‏صراحه  |  ما هو عمرك الحقيقي؟',
+    '‏صراحه  |  ما اكثر شي ندمن عليه؟',
+    'صراحه  |  ما هي أمنياتك المُستقبلية؟‏',
+]
+  client.on('message', message => {
+if (message.content.startsWith('صراحه')) {
+    if(!message.channel.guild) return message.reply('** This command only for servers **');
+ var client= new Discord.RichEmbed()
+ .setTitle("لعبة صراحة ..")
+ .setColor('RANDOM')
+ .setDescription(`${Sra7a[Math.floor(Math.random() * Sra7a.length)]}`)
+ .setImage("https://cdn.discordapp.com/attachments/371269161470525444/384103927060234242/125.png")
+                 .setTimestamp()
+
+  message.channel.sendEmbed(client);
+  message.react("??")
+}
+});  
+
 
 var x1 = "لون 1"
 var x2 = "لون 2"
