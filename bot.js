ad582737870d56539f17ef2cbf5e9a0d55da49fb
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-04-11 20:41:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-    var prefix = ".;
+    var prefix = ".";
     
       if (!message.content.startsWith(prefix)) return;
       var args = message.content.split(' ').slice(1);
@@ -450,7 +450,351 @@ message.delete();
 };     
 });
 
+var prefix = ".";
+var viper = ["https://f.top4top.net/p_682it2tg6.png%22","https://e.top4top.net/p_682a1cus5.png%22","https://d.top4top.net/p_682pycol4.png%22","https://c.top4top.net/p_682vqehy3.png%22","https://b.top4top.net/p_682mlf9d2.png%22","https://a.top4top.net/p_6827dule1.png%22","https://b.top4top.net/p_682g1meb10.png%22","https://a.top4top.net/p_682jgp4v9.png%22","https://f.top4top.net/p_682d4joq8.png%22","https://e.top4top.net/p_6828o0e47.png%22","https://d.top4top.net/p_6824x7sy6.png%22","https://c.top4top.net/p_682gzo2l5.png%22","https://b.top4top.net/p_68295qg04.png%22","https://a.top4top.net/p_682zrz6h3.png%22","https://f.top4top.net/p_6828vkzc2.png%22","https://e.top4top.net/p_682i8tb11.png",]
+    client.on('message', message => {
+        var args = message.content.split(" ").slice(1);
+    if(message.content.startsWith(prefix + 'لو خيروك')) {
+         var lo = new Discord.RichEmbed()
+.setImage(viper[Math.floor(Math.random() * viper.length)])
+message.channel.sendEmbed(lo);
+    }
+});
 
+var UserBlocked = new Set(); // create a new set to save users id.
+
+var prefix = ".";
+var aoasm =[
+    {q:"ما عاصمة **افغانستان**",a:"كبل"},
+    {q:"ما عاصمة ** البانيا**",a:"تيران"},
+    {q:"ما عاصمة **الجزائر **",a:"الجزائر"},
+    {q:"ما عاصمة **جزر القمر**",a:"موروني"},
+    {q:"ما عاصمة **اندورا لا فيلا **",a:"اندورا"},
+    {q:"ما عاصمة **انجولا**",a:"لواندا"},
+    {q:"ما عاصمة **انتيجوا وباربودا**",a:"سان جونز"},
+    {q:"ما عاصمة **الارجنتين**",a:"بوينس ايرس"},
+    {q:"ما عاصمة **ارمينيا**",a:"يريفان"},
+    {q:"ما عاصمة ** مصر**",a:"القاهرة"},
+    {q:"ما عاصمة ** استراليا**",a:"كانبرا"},
+    {q:"ما عاصمة **النمسا**",a:"فيينا"},
+    {q:"ما عاصمة ** اذربيجان**",a:"باكو"},
+    {q:"ما عاصمة **جزر البهاما**",a:"ناساو"},
+    {q:"ما عاصمة **البحرين**",a:"المنامة"},
+    {q:"ما عاصمة ** بنجلاديش**",a:"دكـا"},
+    {q:"ما عاصمة **باربادوس **",a:"بريدجتاون"},
+    {q:"ما عاصمة **بيلا روسيا**",a:"مينسك"},
+    {q:"ما عاصمة ** بلجيكا**",a:"بروكسل"},
+    {q:"ما عاصمة ** بيليز**",a:"بلوم بان"},
+    {q:"ما عاصمة ** بنين**",a:"بورتو نوفو"},
+    {q:"ما عاصمة ** بوتان**",a:"ثيمفو"},
+    {q:"ما عاصمة **بوليفيا **",a:"لاباز"},
+    {q:"ما عاصمة ** البوسنة والهرسك**",a:"سراييفو"},
+    {q:"ما عاصمة ** بوتسوانا**",a:"جابورون"},
+    {q:"ما عاصمة ** البرازيل**",a:"برازيليا"},
+    {q:"ما عاصمة ** بروناى**",a:"بندر سرى بيجاوان"},
+    {q:"ما عاصمة ** بلغاريا**",a:"صوفيا"},
+    {q:"ما عاصمة ** بوركينا فاسو**",a:"واجادوجو"},
+    {q:"ما عاصمة **بوروندى **",a:"بوجومبورا"},
+    {q:"ما عاصمة **كمبوديا **",a:"بنوم بنـه"},
+    {q:"ما عاصمة ** الكاميرون**",a:"ياوندى"},
+    {q:"ما عاصمة ** كندا**",a:"اوتاوا"},
+    {q:"ما عاصمة ** الرأس الاخضر**",a:"برايا"},
+    {q:"ما عاصمة **تشاد **",a:"نجامينا"},
+    {q:"ما عاصمة ** شيلى**",a:"سانتياجو"},
+    {q:"ما عاصمة **الصين **",a:"بكين"},
+    {q:"ما عاصمة ** **",a:"مورونى"},
+    {q:"ما عاصمة **كوستاريكا **",a:"سان خوسيه"},
+    {q:"ما عاصمة ** كوت ديفوار**",a:"ابيدجان"},
+    {q:"ما عاصمة **كرواتيا **",a:"زغرب"},
+    {q:"ما عاصمة ** كوبا**",a:"هافانا"},
+    {q:"ما عاصمة ** قبرص**",a:" "},
+    {q:"ما عاصمة ** جمهورية التشيك**",a:"براغ"},
+    {q:"ما عاصمة **الدنمارك **",a:"كوبنهاجن"},
+    {q:"ما عاصمة ** جيبوتى**",a:"جيبوتى"},
+    {q:"ما عاصمة ** دومينيكا**",a:"روسيو"},
+    {q:"ما عاصمة **الدومينيكان **",a:"سان دومينجو"},
+    {q:"ما عاصمة **تيمور الشرقية **",a:"ديلى"},
+    {q:"ما عاصمة **قطر  **",a:"الدوحة"},
+    {q:"ما عاصمة **السعودية  **",a:"الرياض"},
+    {q:"ما عاصمة **سوريا  **",a:"دمشق"},
+    {q:"ما عاصمة **تركيا  **",a:"انقرة"},
+    {q:"ما عاصمة **العراق  **",a:"بغداد"},
+    {q:"ما عاصمة **البنان  **",a:"بيروت"},
+    {q:"ما عاصمة **فلسطين  **",a:"القدس"},
+    {q:"ما عاصمة **امريكا  **",a:"واشنطن"},
+    {q:"ما عاصمة **الاردن  **",a:"عمان"},    
+    {q:"ما عاصمة **السودان  **",a:"خرطوم"},
+    {q:"ما عاصمة **المانيا  **",a:"برلين"},
+    {q:"ما عاصمة **كندا  **",a:"اوتاوا"},
+    {q:"ما عاصمة **البرازيل  **",a:"برازيليا"},
+    
+    
+    
+   ];
+   client.on("message", async message => {
+    if(message.content == prefix+"عواصم"){
+        if(UserBlocked.has(message.guild.id)) return message.channel.send("هناك جلسة .")
+        UserBlocked.add(message.guild.id)
+        var ask = aoasm[Math.floor(Math.random() * aoasm.length)];
+        let embed = new Discord.RichEmbed()
+        .setTitle('سؤال عواصم')
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setColor("RANDOM")
+        .setDescription(ask.q);
+        message.channel.sendEmbed(embed).then(msg=> msg.delete(20000))
+        const msgs = await message.channel.awaitMessages(msg => msg.author.id !== client.user.id ,{maxMatches:1,time:10000});
+            UserBlocked.delete(message.guild.id)
+        msgs.forEach(result => {
+           if(result.author.id == client.user.id) return;
+           if(result.content == "عاصمة") return
+           if(result.content == ask.a){
+
+             let embeds = new Discord.RichEmbed()
+             .setTitle(':white_check_mark: اجابة صحيحة')
+             .setAuthor(message.author.username, message.author.avatarURL)
+             .setColor("RANDOM")
+             .setDescription(`**${result.author.username}** الإجابة صحيحة`);
+                message.channel.sendEmbed(embeds);                return;
+           } else {
+
+                                  var embedx = new Discord.RichEmbed()
+                .setTitle(':x:خطاء')
+                .setAuthor(message.author.username, message.author.avatarURL)
+                .setColor("RANDOM")
+                .setDescription(`**${result.author.username}** الإجابة خاطئة`);
+                message.channel.sendEmbed(embedx);
+           }
+     });
+  }
+});
+
+var prefix = ".";
+var fkk =[
+        {f:"فكك بسم الله الرحمن الرحيم",k:"ب س م ا ل ل ه ا ل ر ح م ن ا ل ر ح ي م"},
+        {f:"فكك باص",k:"ب ا ص"},
+        {f:"فكك عربة ",k:"ع ر ب ة"},
+        {f:"فكك سيارة",k:"س ي ا ر ة"},
+        {f:"فكك سيرفرنا احلى سيرفر",k:"س ي ر ف ر ن ا ا ح ل ى س ي ر ف ر"},
+        {f:"فكك العنود ",k:"ا ل ع ن و د"},
+        {f:"فكك المستتكعكبتيه",k:"ا ل م س ت ت ك ع ك ب ت ي ه"},
+        {f:"فكك دحوم",k:"د ح و م"},
+        {f:"فكك اونرنا احلى اونر",k:"ا و ن ر ن ا ا ح ل ى ا و ن ر"},
+        {f:"فكك الحياة حلوة",k:"ا ل ح ي ا ة ح ل و ة"},
+        {f:"فكك كازخستان ",k:"ك ا ز خ س ت ا ن"},
+        {f:"لحم الحمام حلال ولحم الحمار حرام ",k:"ل ح م ا ل ح م ا م ح ل ا ل و ل ح م ا ل ح م ا ر ح ر ا م"},
+        {f:"فكك استونيا ",k:"ا س ت و ن ي ا"},
+        {f:"فكك لقمة وجغمه ",k:"ل ق م ة و ج غ م ه"},
+        {f:"فكك زنديق  ",k:"ز ن د ي ق"},
+        {f:"فكك استراليا ",k:"ا س ت ر ا ل ي ا"},
+        {f:"فكك سوريا ",k:"س و ر ي ا"},
+        {f:"فكك الاردن ",k:"ا ل ا ر د ن"},
+        {f:"فكك طماطم ",k:"ط م ا ط م"},
+        {f:"فكك سارة ",k:"س ا ر ة"},
+        {f:"فكك دراجون ",k:"د ر ا ج و ن"},
+        {f:"فكك سيرفر ",k:"س ي ر ف ر"},
+        {n:"فكك الجبل",m:"ا ل ج ب ل"},
+        {n:"فكك هضبة",m:"ه ض ب ة"},
+        {n:"فكك خواطر",m:"خ و ا ط ر"},
+        {n:"فكك ارحبو",m:"ا ر ح ب و"},
+        {n:"فكك اطنخ سيرفر",m:"ا ط ن خ س ي ف ر"},
+        {n:"فكك احبك",m:"ا ح ب ك"},
+        {n:"فكك سبرايز",m:"س ب ر ا ي ز"},
+        {n:"فكك ولي على أمتك",m:"و ل ي ع ل ى أ م ت ك"},
+
+
+
+   ];
+
+
+   client.on("message", async message => {
+    if(message.content == prefix+"فكك"){
+        if(UserBlocked.has(message.guild.id)) return message.channel.send("هناك جلسة .")
+        UserBlocked.add(message.guild.id)
+        var ask = fkk[Math.floor(Math.random() * fkk.length)];
+        let embed = new Discord.RichEmbed()
+        .setTitle('لعبة فكك')
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setColor("RANDOM")
+        .setDescription(ask.f);
+        message.channel.sendEmbed(embed).then(msg=> msg.delete(200000))
+        const msgs = await message.channel.awaitMessages(msg => msg.author.id !== client.user.id ,{maxMatches:1,time:100000});
+            UserBlocked.delete(message.guild.id)
+        msgs.forEach(result => {
+           if(result.author.id == client.user.id) return;
+           if(result.content == "فكك") return
+           if(result.content == ask.k){
+
+             let embeds = new Discord.RichEmbed()
+             .setTitle(':white_check_mark: اجابة صحيحة')
+             .setAuthor(message.author.username, message.author.avatarURL)
+             .setColor("RANDOM")
+             .setDescription(`**${result.author.username}** الإجابة صحيحة`);
+                message.channel.sendEmbed(embeds);                return;
+           } else {
+
+                               var embedx = new Discord.RichEmbed()
+             .setTitle(':x:خطاء')
+             .setAuthor(message.author.username, message.author.avatarURL)
+             .setColor("RANDOM")
+             .setDescription(`**${result.author.username}** الإجابة خاطئة`);
+
+                message.channel.sendEmbed(embedx);
+           }
+     });
+  }
+});
+
+const secreT = [
+    "**الحياة بكل ما فيها تقف دائمًا على حد الوسطية بين اتزان المعنى وضده من حب وكره وحق وباطل وعدل وظلم**.",
+    "**كى تعيش عليك ان تتقن فن التجاهل باحتراف**.",
+    "**لا تحزن على من اشعرك بان طيبتك غباء امام وقاحته**.",
+    "**هناك من يحلم بالنجاح وهناك من يستيقظ باكرا لتحقيقه**.",
+    "**ان تعالج ألمك بنفسك تلك هى القوة**.", 
+    "**الجميع يسمع ما تقول والاصدقاء ينصتون لما تقول وافضل الاصدقاء ينصتون لما اخفاه سكوتك**.", 
+    "**انتهى زمن الفروسية ، لم تنقرض الخيول بل انقرض الفرسان**.", 
+    "**ان تكون اخرسا عاقلا خير من ان تكون نطوقا جهولا**.", 
+    "**المناقشات العقيمة لا تنجب افكارا**.", 
+    "**نحن نكتب ما لا نستطيع ان نقول وما نريد ان يكون**.", 
+    "**نحن نكتب ما لا نستطيع ان نقول وما نريد ان يكون**.", 
+  ]
+  
+  
+   client.on('message', message => {
+     if (message.content.startsWith("خواطر")) {
+                  if(!message.channel.guild) return message.reply('** This command only for servers**');
+    var embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+  
+     .setThumbnail(message.author.avatarURL) 
+   .addField('لعبه خواطر' ,
+    `${secreT[Math.floor(Math.random() * secreT.length)]}`)
+    message.channel.sendEmbed(embed);
+    console.log('[id] Send By: ' + message.author.username)
+      }
+  });
+
+
+
+const cuttweet = [
+    'كت تويت ‏| تخيّل لو أنك سترسم شيء وحيد فيصبح حقيقة، ماذا سترسم؟',
+    'كت تويت | أكثر شيء يُسكِت الطفل برأيك؟',
+    'كت تويت | الحرية لـ ... ؟',
+    'كت تويت | قناة الكرتون المفضلة في طفولتك؟',
+    'كت تويت ‏| كلمة للصُداع؟',
+    'كت تويت ‏| ما الشيء الذي يُفارقك؟',
+    'كت تويت | موقف مميز فعلته مع شخص ولا يزال يذكره لك؟',
+    'كت تويت ‏| أيهما ينتصر، الكبرياء أم الحب؟',
+    'كت تويت | بعد ١٠ سنين ايش بتكون ؟',
+    'كت تويت ‏| مِن أغرب وأجمل الأسماء التي مرت عليك؟',
+    '‏كت تويت | عمرك شلت مصيبة عن شخص برغبتك ؟',
+    'كت تويت | أكثر سؤال وجِّه إليك مؤخرًا؟',
+    '‏كت تويت | ما هو الشيء الذي يجعلك تشعر بالخوف؟',
+    '‏كت تويت | وش يفسد الصداقة؟',
+    '‏كت تويت | شخص لاترفض له طلبا ؟',
+    '‏كت تويت | كم مره خسرت شخص تحبه؟.',
+    '‏كت تويت | كيف تتعامل مع الاشخاص السلبيين ؟',
+    '‏كت تويت | كلمة تشعر بالخجل اذا قيلت لك؟',
+    '‏كت تويت | جسمك اكبر من عٌمرك او العكسّ ؟!',
+    '‏كت تويت |أقوى كذبة مشت عليك ؟',
+    '‏كت تويت | تتأثر بدموع شخص يبكي قدامك قبل تعرف السبب ؟',
+    'كت تويت | هل حدث وضحيت من أجل شخصٍ أحببت؟',
+    '‏كت تويت | أكثر تطبيق تستخدمه مؤخرًا؟',
+    '‏كت تويت | ‏اكثر شي يرضيك اذا زعلت بدون تفكير ؟',
+    '‏كت تويت | وش محتاج عشان تكون مبسوط ؟',
+    '‏كت تويت | مطلبك الوحيد الحين ؟',
+    '‏كت تويت | هل حدث وشعرت بأنك ارتكبت أحد الذنوب أثناء الصيام؟',
+]
+
+client.on('message', message => {
+  if (message.content.startsWith("كت تويت")) {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+ var embed = new Discord.RichEmbed()
+ .setColor('RANDOM')
+  .setThumbnail(message.author.avatarURL) 
+.addField('لعبه كت تويت' ,
+ `${cuttweet[Math.floor(Math.random() * cuttweet.length)]}`)
+ message.channel.sendEmbed(embed);
+ console.log('[id] Send By: ' + message.author.username)
+   }
+});
+
+
+
+const Sra7a = [
+    'صراحه  |  صوتك حلوة؟',
+    'صراحه  |  التقيت الناس مع وجوهين؟',
+    'صراحه  |  شيء وكنت تحقق اللسان؟',
+    'صراحه  |  أنا شخص ضعيف عندما؟',
+    'صراحه  |  هل ترغب في إظهار حبك ومرفق لشخص أو رؤية هذا الضعف؟',
+    'صراحه  |  يدل على أن الكذب مرات تكون ضرورية شي؟',
+    'صراحه  |  أشعر بالوحدة على الرغم من أنني تحيط بك كثيرا؟',
+    'صراحه  |  كيفية الكشف عن من يكمن عليك؟',
+    'صراحه  |  إذا حاول شخص ما أن يكرهه أن يقترب منك ويهتم بك تعطيه فرصة؟',
+    'صراحه  |  أشجع شيء حلو في حياتك؟',
+    'صراحه  |  طريقة جيدة يقنع حتى لو كانت الفكرة خاطئة" توافق؟',
+    'صراحه  |  كيف تتصرف مع من يسيئون فهمك ويأخذ على ذهنه ثم ينتظر أن يرفض؟',
+    'صراحه  |  التغيير العادي عندما يكون الشخص الذي يحبه؟',
+    'صراحه  |  المواقف الصعبة تضعف لك ولا ترفع؟',
+    'صراحه  |  نظرة و يفسد الصداقة؟',
+    'صراحه  |  ‏‏إذا أحد قالك كلام سيء بالغالب وش تكون ردة فعلك؟',
+    'صراحه  |  شخص معك بالحلوه والمُره؟',
+    'صراحه  |  ‏هل تحب إظهار حبك وتعلقك بالشخص أم ترى ذلك ضعف؟',
+    'صراحه  |  تأخذ بكلام اللي ينصحك ولا تسوي اللي تبي؟',
+    'صراحه  |  وش تتمنى الناس تعرف عليك؟',
+    'صراحه  |  ابيع المجرة عشان؟',
+    'صراحه  |  أحيانا احس ان الناس ، كمل؟',
+    'صراحه  |  مع مين ودك تنام اليوم؟',
+    'صراحه  |  صدفة العمر الحلوة هي اني؟',
+    'صراحه  |  الكُره العظيم دايم يجي بعد حُب قوي " تتفق؟',
+    'صراحه  |  صفة تحبها في نفسك؟',
+    'صراحه  |  ‏الفقر فقر العقول ليس الجيوب " ، تتفق؟',
+    'صراحه  |  تصلي صلواتك الخمس كلها؟',
+    'صراحه  |  ‏تجامل أحد على راحتك؟',
+    'صراحه  |  اشجع شيء سويتة بحياتك؟',
+    'صراحه  |  وش ناوي تسوي اليوم؟',
+    'صراحه  |  وش شعورك لما تشوف المطر؟',
+    'صراحه  |  غيرتك هاديه ولا تسوي مشاكل؟',
+    'صراحه  |  ما اكثر شي ندمن عليه؟',
+    'صراحه  |  اي الدول تتمنى ان تزورها؟',
+    'صراحه  |  متى اخر مره بكيت؟',
+    'صراحه  |  تقيم حظك ؟ من عشره؟',                                                                                                                                            
+  'صراحه  |  هل تعتقد ان حظك سيئ؟',
+    'صراحه  |  شـخــص تتمنــي الإنتقــام منـــه؟',
+    'صراحه  |  كلمة تود سماعها كل يوم؟',
+    'صراحه  |  **هل تُتقن عملك أم تشعر بالممل؟',
+    'صراحه  |  هل قمت بانتحال أحد الشخصيات لتكذب على من حولك؟',
+    'صراحه  |  متى آخر مرة قمت بعمل مُشكلة كبيرة وتسببت في خسائر؟',
+    'صراحه  |  ما هو اسوأ خبر سمعته بحياتك؟',
+    '‏صراحه | هل جرحت شخص تحبه من قبل ؟',
+    'صراحه  |  ما هي العادة التي تُحب أن تبتعد عنها؟',
+    '‏صراحه | هل تحب عائلتك ام تكرههم؟',
+    '‏صراحه  |  من هو الشخص الذي يأتي في قلبك بعد الله – سبحانه وتعالى- ورسوله الكريم – صلى الله عليه وسلم؟',
+    '‏صراحه  |  هل خجلت من نفسك من قبل؟',
+    '‏صراحه  |  ما هو ا الحلم  الذي لم تستطيع ان تحققه؟',
+    '‏صراحه  |  ما هو الشخص الذي تحلم به كل ليلة؟',
+    '‏صراحه  |  هل تعرضت إلى موقف مُحرج جعلك تكره صاحبهُ؟',
+     '‏صراحه  |  هل قمت بالبكاء أمام من تُحب؟',
+    '‏صراحه  |  ماذا تختار حبيبك أم صديقك؟',
+    '‏صراحه  | هل حياتك سعيدة أم حزينة؟',
+    'صراحه  |  ما هي أجمل سنة عشتها بحياتك؟',
+    '‏صراحه  |  ما هو عمرك الحقيقي؟',
+    '‏صراحه  |  ما اكثر شي ندمن عليه؟',
+    'صراحه  |  ما هي أمنياتك المُستقبلية؟‏',
+]
+  client.on('message', message => {
+if (message.content.startsWith('صراحه')) {
+    if(!message.channel.guild) return message.reply('** This command only for servers **');
+ var client= new Discord.RichEmbed()
+ .setTitle("لعبة صراحة ..")
+ .setColor('RANDOM')
+ .setDescription(`${Sra7a[Math.floor(Math.random() * Sra7a.length)]}`)
+ .setImage("https://cdn.discordapp.com/attachments/371269161470525444/384103927060234242/125.png")
+                 .setTimestamp()
+
+  message.channel.sendEmbed(client);
+  message.react("??")
+}
+});  
 
 var x1 = "لون 1"
 var x2 = "لون 2"
