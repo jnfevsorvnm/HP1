d32d2ac672522f3472aaa7335ea00b76b43feea1
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-04-11 20:43:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const moment = require('moment');
+const fs = require("fs");
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
