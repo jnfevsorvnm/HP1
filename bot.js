e462ef03b523b1f6f9fa27360786c714b6cb5cee
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-04-16 04:14:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -396,7 +396,7 @@ if(!message.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return m
 
  client.on('message', message => {
               if(!message.channel.guild) return;
-    var prefix = "!";
+    var prefix = "+";
     if(message.content.startsWith(prefix + 'bc')) {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
