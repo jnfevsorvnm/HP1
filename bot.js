542bb9c4e9507d0116851570c28df899cd7695ec
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-04-21 18:58:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -130,6 +130,92 @@ client.on("guildMemberAdd", (member) => {
         })
     })
 });
+let points = JSON.parse(fs.readFileSync('./fkk/3wasmPTS.json', 'utf8'));
+     
+var prefix = ".";
+
+client.on('message', message => {
+if (!points[message.author.id]) points[message.author.id] = {
+	points: 0,
+  };
+if (message.content.startsWith(prefix + 'عواصم')) {
+	if(!message.channel.guild) return
+
+const type = require('./fkk/3wasm.json');
+const item = type[Math.floor(Math.random() * type.length)];
+const filter = response => {
+    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+};
+message.channel.send('**لديك 10 ثانية لتجيب**').then(msg => {
+
+			
+msg.channel.send(`${item.type}`).then(() => {
+        message.channel.awaitMessages(filter, { maxMatches: 1, time: 10000, errors: ['time'] })
+        .then((collected) => {
+		message.channel.send(`${collected.first().author} ✅ **مبروك لقد كسبت نقطه
+لمعرفة نقطاك الرجاء كتابة .نقاطي**`);
+		console.log(`[Typing] ${collected.first().author} typed the word.`);
+			let userData = points[message.author.id];
+			userData.points++;
+          })
+          .catch(collected => {
+            message.channel.send(`:x: **خطأ حاول مرة اخرى**`);
+			console.log('[Typing] Error: No one type the word.');
+          })
+		})
+	})
+}
+});
+
+client.on('message', message => {
+if (!points[message.author.id]) points[message.author.id] = {
+	points: 0,
+  };
+if (message.content.startsWith(prefix + 'فكك')) {
+	if(!message.channel.guild) return
+
+const type = require('./fkk/fkkk.json');
+const item = type[Math.floor(Math.random() * type.length)];
+const filter = response => {
+    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+};
+message.channel.send('**لديك 15 ثانية لتجيب**').then(msg => {
+
+			
+msg.channel.send(`${item.type}`).then(() => {
+        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
+        .then((collected) => {
+		message.channel.send(`${collected.first().author} ✅ **مبروك لقد كسبت نقطه
+لمعرفة نقطاك الرجاء كتابة .نقاطي**`);
+		console.log(`[Typing] ${collected.first().author} typed the word.`);
+			let userData = points[message.author.id];
+			userData.points++;
+          })
+          .catch(collected => {
+            message.channel.send(`:x: **خطأ حاول مرة اخرى**`);
+			console.log('[Typing] Error: No one type the word.');
+          })
+		})
+	})
+}
+});
+
+
+
+client.on('message', message => {
+if (message.content.startsWith(prefix + 'نقاطي')) {
+	if(!message.channel.guild) return
+	let userData = points[message.author.id];
+	let embed = new Discord.RichEmbed()
+    .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+	.setColor('#000000')
+	.setDescription(`نقاطك: \`${userData.points}\``)
+	message.channel.sendEmbed(embed)
+  }
+  fs.writeFile("./l3b/3wasmPTS.json", JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+  })
+});
 
 client.on('message', message => {
     var prefix = ".";
