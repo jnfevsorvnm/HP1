bb160b4d5241b0186a85c221eb90b4b8cde86b88
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-04-25 05:07:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -142,7 +142,25 @@ client.on("message", message => {
     } 
 });
 
-
+client.on('message', message => { 
+    var prefix = "#";
+    if (message.author.boss) return;
+    if (!message.content.startsWith(prefix)) return;
+    let command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+    if (command == "role") {
+    if (!message.channel.guild) return;
+    if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return message.reply("**:no_entry_sign:انت لا تملك صلاحيات**").then(msg => msg.delete(5000));;
+    if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.reply("البوت لايملك صلاحيات").then(msg => msg.delete(5000));;
+    let user = message.mentions.users.first();
+    if (message.mentions.users.size < 1) return message.reply('**ضع منشن الشخص!!**').then(msg => {msg.delete(5000)});
+    let MRole = message.content.split(" ").slice(2).join(" ");
+    if(!MRole)return message.reply("يجب عليك وضع اسم الرتبة").then(msg => {msg.delete(5000)});
+    if (!message.guild.member(user).roleable) return message.reply("**لايمكنني اعطاء شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**").then(msg => {msg.delete(5000)});
+    message.guild.member(user).addRole(message.guild.roles.find("name", MRole));
+    message.reply('***تم اعطاء الشخص رتبة بنجاح:white_check_mark:***').then(msg => {msg.delete(10000)});
+    }
+    });
 
 client.on('message', message => { 
         var prefix = "ا";                     
