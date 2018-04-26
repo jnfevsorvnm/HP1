70f107bae6a4755ad4d6ccf323330cb1ddf95445
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-04-26 21:10:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1217,12 +1217,13 @@ if(!message.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return m
 
 
 client.on('message', message => {
+    var prefix = ".";
     if(!message.channel.guild) return;
-if(message.content.startsWith(prefix + '.bc')) {
+if(message.content.startsWith(prefix + 'bc')) {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-if(!message.member.hasPermission('MANAGE_SERVER')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `MANAGE_SERVER`' );
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**هذة الأمر للأدارة فقط**');
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-let copy = "Test";
+let copy = "HP CLAN";
 let request = `Requested By ${message.author.username}`;
 if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
 msg.react('✅')
