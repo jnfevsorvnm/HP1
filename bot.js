baf827e856d7521efe9cf56de80dd2db08a720c0
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-04-29 01:06:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -454,6 +454,34 @@ if(!message.channel.guild) return;
      modlog2.send({embed:heroo});
 });
 
+              client.on('message', message => {
+               if (message.content.startsWith(".user")) {
+         var args = message.content.split(" ").slice(1);
+         let user = message.mentions.users.first();
+         var men = message.mentions.users.first();
+            var heg;
+            if(men) {
+                heg = men
+            } else {
+                heg = message.author
+            }
+          var mentionned = message.mentions.members.first();
+             var h;
+            if(mentionned) {
+                h = mentionned
+            } else {
+                h = message.member
+            }
+                   moment.locale('ar-TN');
+          var id = new  Discord.RichEmbed()
+        .setColor("RANDOM")
+        .addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)
+        .addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true)
+       .setThumbnail(message.author.avatarURL)
+        message.channel.send(id)
+    }       });
+
+
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(` ═════════════════════ஜ۩۞۩ஜ═════════════════════ 
