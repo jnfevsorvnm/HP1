bfaa89d17f137196749d03f08cb3e0b1bb01210c
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-05-02 21:02:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -117,29 +117,7 @@ hours = currentTime.getHours() + 3 ,
     }
   });
 
-client.on('ready', () => {
-    
-               let currentTime = new Date(),
-            h = currentTime.getHours() + 3 ,
-            m = currentTime.getMinutes()+1-1,
-            s = currentTime.getSeconds()
 
-            if (m < 10) {
-                m = "0" + m;
-            }
-            var suffix = "AM";
-            if (h >= 12) {
-                suffix = "PM";
-                h = h - 12;
-            }
-            if (h == 0) {
-                h = 12;
-            }
-    setInterval(() => {
-        if(!true) return;
-            client.channels.get(`439829759304663041`).edit({name: `Time Now is: ${h}:${m} ${suffix}.`});
-    },1*1000)
-})
 
 client.on("message", message => { 
     var prefix = ".";
